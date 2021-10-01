010fe3b263ec65c92a5e1435a9df729d853628df
ezgiaslan <ezgiasl08@gmail.com>
2021-10-01 15:58:29 +0300

    Update

--- console/Program.cs
+++ console/Program.cs
@@ -7,6 +7,12 @@ namespace console
         static void Main(string[] args) 
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi Giriniz:");
+            string name =Console.ReadLine();
+            Console.WriteLine("Soyadınızı Giriniz:");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba" + " " + name + " " + surname);
         }
     }
 }
